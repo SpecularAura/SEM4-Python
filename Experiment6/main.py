da0fa9e1dfcfc521a20ec7cb2042e07a22f93263
Aum Kulkarni <aumwinay@gmail.com>
2023-03-21 00:49:04 +0530

    Basic Tkinter Done

--- Experiment6/main.py
+++ Experiment6/main.py
@@ -36,18 +36,59 @@ class LoginWindow(tk.Frame):
         self.secret_txt = tk.Text(self, width=35, height=5, wrap=tk.WORD)
         self.secret_txt.grid(row=6, column=0, columnspan=2, sticky=tk.W)
     def newuser(self):
-        self.master.withdraw()
+        # self.master.withdraw()
         new_window = tk.Toplevel(self.master)
         new_window.title("Welcome!")
-        tk.Label(new_window, text="Hello To User Page!").grid()
-        pass
+        
+        # Username label and entry
+        self.new_username_label = tk.Label(new_window, text="Username")
+        self.new_username_label.grid()
+        self.new_username_entry = tk.Entry(new_window)
+        self.new_username_entry.grid()
+
+        # Password label and entry
+        self.new_password_label = tk.Label(new_window, text="Password")
+        self.new_password_label.grid()
+        self.new_password_entry = tk.Entry(new_window, show="*")
+        self.new_password_entry.grid()
+
+        # Login button
+        self.new_login_button = tk.Button(new_window, text="Create User", command=lambda: self.createUser(new_window))
+        self.new_login_button.grid()
+
+    def createUser(self, new_window):
+        with open(r'userdata.txt', "a") as file1:
+            username = self.new_username_entry.get()
+            password = self.new_password_entry.get()
+            if self.validCredentials(username, password):
+                file1.write(f'{username}:{password},')
+        new_window.withdraw()
+
+    # Checks if the new user 
+    def validCredentials(username, password):
+        if password.length < 8: 
+            return False
+        return True 
+
+    def isValidUser(self, entered_user_name, entered_pasword):
+        with open(r'userdata.txt', 'r') as file1:
+            for line in file1:
+                for user in line.split(','):
+                    print(user)
+                    username, password = user.split(':')
+                    if username == entered_user_name and password == entered_pasword:
+                        return True
+        return False
+
+
     def login(self):
         message = ""
         # Check the credentials
-        if self.username_entry.get() == "user" and self.password_entry.get() == "pass":
+        # if self.username_entry.get() == "user" and self.password_entry.get() == "pass":
+        if self.isValidUser(self.username_entry.get(), self.password_entry.get()):
             # If the credentials are correct, open a new window
-            self.master.withdraw()
-            new_window = tk.Toplevel(self.master)
+            self.master.destroy()
+            new_window = tk.Tk()
             new_window.title("Welcome!")
             tk.Label(new_window, text="Welcome!").grid()
 
@@ -65,6 +106,6 @@ class LoginWindow(tk.Frame):
 
 
 root = tk.Tk()
-root.geometry("200x85")
+root.geometry("360x360")
 app = LoginWindow(master=root)
 app.mainloop()
