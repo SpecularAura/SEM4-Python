7cc6921354f50a4b15ec769bf24f79e9a75879e9
Aum Kulkarni <aumwinay@gmail.com>
2023-03-29 00:41:19 +0530

    DOne with Experiment6

--- Experiment6/Experiment6.md
+++ Experiment6/Experiment6.md
@@ -1,1 +1,20 @@
-image.png
+---
+header-includes:
+ - \usepackage{fontspec}
+ - \usepackage{fvextra}
+ - \setmainfont{Times New Roman}
+ - \setmonofont{Consolas}
+ - \fvset{breaklines=true, breakanywhere=true}
+ - \renewcommand{\theFancyVerbLine}{\textcolor[rgb]{0.0,0.0,0.0}{\small\arabic{FancyVerbLine}}}
+ - \DefineVerbatimEnvironment{Highlighting}{Verbatim}{commandchars=\\\{\}, frame=leftline, numbersep=4pt, framesep=4pt}
+fontsize: 12pt
+geometry: left=2cm,right=2cm,top=2cm,bottom=2cm
+---
+Name: Aum Kulkarni  
+RollNo: 36  
+Div: D6AD  
+**Experiment5**:
+```{.py include="main.py" .numberLines}
+```
+**Output:**
+![la lune](Screenshot 2023-03-29 002553.png)
--- Experiment6/main.py
+++ Experiment6/main.py
@@ -12,47 +12,38 @@ class LoginWindow(tk.Frame):
         self.master.title("Login")
         self.pack()
 
-        # Username label and entry
         self.username_label = tk.Label(self, text="Username")
         self.username_label.grid()
         self.username_entry = tk.Entry(self)
         self.username_entry.grid()
 
-        # Password label and entry
         self.password_label = tk.Label(self, text="Password")
         self.password_label.grid()
         self.password_entry = tk.Entry(self, show="*")
         self.password_entry.grid()
 
-        # Login button
         self.login_button = tk.Button(self, text="Login", command=self.login)
         self.login_button.grid()
 
-        # Add User Button
         self.add_user_button = tk.Button(self, text="New User", command=self.newuser)
         self.add_user_button.grid()
 
-        #Details
         self.secret_txt = tk.Text(self, width=35, height=5, wrap=tk.WORD)
         self.secret_txt.grid(row=6, column=0, columnspan=2, sticky=tk.W)
     def newuser(self):
-        # self.master.withdraw()
         new_window = tk.Toplevel(self.master)
         new_window.title("Welcome!")
         
-        # Username label and entry
         self.new_username_label = tk.Label(new_window, text="Username")
         self.new_username_label.grid()
         self.new_username_entry = tk.Entry(new_window)
         self.new_username_entry.grid()
 
-        # Password label and entry
         self.new_password_label = tk.Label(new_window, text="Password")
         self.new_password_label.grid()
         self.new_password_entry = tk.Entry(new_window, show="*")
         self.new_password_entry.grid()
 
-        # Login button
         self.new_login_button = tk.Button(new_window, text="Create User", command=lambda: self.createUser(new_window))
         self.new_login_button.grid()
 
@@ -64,7 +55,6 @@ class LoginWindow(tk.Frame):
                 file1.write(f'{username}:{password},')
         new_window.withdraw()
 
-    # Checks if the new user 
     def validCredentials(username, password):
         if password.length < 8: 
             return False
@@ -74,32 +64,28 @@ class LoginWindow(tk.Frame):
         with open(r'userdata.txt', 'r') as file1:
             for line in file1:
                 for user in line.split(','):
-                    print(user)
-                    username, password = user.split(':')
-                    if username == entered_user_name and password == entered_pasword:
-                        return True
+                    if user != '':
+                        print(user)
+                        print(user.split(':'))
+                        username, password = user.split(':')
+                        if username == entered_user_name and password == entered_pasword:
+                            return True
         return False
 
 
     def login(self):
         message = ""
-        # Check the credentials
-        # if self.username_entry.get() == "user" and self.password_entry.get() == "pass":
         if self.isValidUser(self.username_entry.get(), self.password_entry.get()):
-            # If the credentials are correct, open a new window
             self.master.destroy()
             new_window = tk.Tk()
             new_window.title("Welcome!")
             tk.Label(new_window, text="Welcome!").grid()
 
         else:
-            # If the credentials are incorrect, increment the attempts counter
             self.attempts += 1
             if self.attempts >= 3:
-                # If there have been 3 or more failed attempts, exit the program
                 self.master.quit()
             else:
-                # Otherwise, display an error message
                 message = "Error: " + "Incorrect username or password."
                 self.secret_txt.delete(0.0, tk.END)
                 self.secret_txt.insert(0.0, message)
